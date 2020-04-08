7f0a699b5ac5df18e51bafa2cc360b9298db2d7a
zyl <zyl@localhost.com>
2020-04-08 17:14:43 +0800

    add prometheus

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 
-_PORT = 9080 if (os.environ.get("PORT") is None) else os.environ.get("PORT")
+_port = 9080 if (os.environ.get("PORT") is None) else os.environ.get("PORT")
 
 app = Flask(__name__)
 
@@ -10,4 +10,4 @@ def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(host='::', port=_PORT, debug=True, threaded=True)
+    app.run(host='::', port=_port, debug=True, threaded=True)
