be4f194f54d2e1419a846da669ac01e0bf648f16
zyl <zyl@localhost.com>
2020-04-09 20:19:58 +0800

    ff

--- hello.py
+++ hello.py
@@ -1,13 +1,18 @@
 import os
+import time
+import random
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 _port = 9080 if (os.environ.get("PORT") is None) else os.environ.get("PORT")
 
 app = Flask(__name__)
+PrometheusMetrics(app)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    time.sleep(random.random())
+    return "hello world"
 
 if __name__ == "__main__":
-    app.run(host='::', port=_port, debug=True, threaded=True)
+    app.run(host='::', port=_port, threaded=True)
---
+++ hello_prometheus_client.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, Response
+
+import prometheus_client
+from prometheus_client.core import CollectorRegistry
+
+_port = 9080 if (os.environ.get("PORT") is None) else os.environ.get("PORT")
+
+REGISTRY = CollectorRegistry(auto_describe=False)
+flask_http_request_total = prometheus_client.Counter("flask_http_request_total", 
+  "flask http request total", ['method', 'endpoint'], registry=REGISTRY)
+flask_http_request_duration_seconds = prometheus_client.Histogram("flask_http_request_duration_seconds",
+  "flask http request duration seconds", registry=REGISTRY)
+
+app = Flask(__name__)
+
+@app.route('/')
+@flask_http_request_duration_seconds.time()
+def hello():
+    flask_http_request_total.labels(method='get', endpoint='/').inc()
+    return "hello world"
+
+@app.route("/metrics")
+def requests_count():
+    return Response(prometheus_client.generate_latest(REGISTRY),
+                    mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(host='::', port=_port, debug=True, threaded=True)
