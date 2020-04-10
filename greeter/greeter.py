e4bb84f326cb9291451f0ede31ab041e91e4dfe6
zyl <zyl@localhost.com>
2020-04-10 10:55:23 +0800

    f

---
+++ greeter/greeter.py
@@ -0,0 +1,15 @@
+import os
+import requests
+from flask import Flask, request
+
+_port = 9082 if (os.environ.get("PORT") is None) else os.environ.get("PORT")
+
+app = Flask(__name__)
+
+@app.route('/')
+def greeter():
+    hello_to = request.args.get('helloTo')
+    return 'Http Hello, %s!' % hello_to
+
+if __name__ == "__main__":
+    app.run(host='::', port=_port, debug=True, threaded=True)
