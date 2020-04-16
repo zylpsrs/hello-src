7a69aef6dda4f26213c99606fe41bfca2cca4b2b
zyl <zyl@localhost.com>
2020-04-16 11:25:27 +0800

    f

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask
 
-_PORT = 9080 if (os.environ.get("PORT") is None) else os.environ.get("PORT")
+_port = 9080 if (os.environ.get("PORT") is None) else os.environ.get("PORT")
+_msg  = "Hello, World!" if (os.environ.get("MESSAGE") is None) else os.environ.get("MESSAGE")
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "{}".format(_msg)
 
 if __name__ == "__main__":
-    app.run(host='::', port=_PORT, debug=True, threaded=True)
+    app.run(host='::', port=_port, debug=True, threaded=True)
